f1140735c92e9ecb26581c9f48de5e46227375bf
roger.mandeville <roger.mandeville@mymail.champlain.edu>
2023-11-17 06:02:57 -0500

    finished assignment 7

--- assignments/assignment7_lighting/main.cpp
+++ assignments/assignment7_lighting/main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <string>
+#include <sstream>
 
 #include <ew/external/glad.h>
 #include <ew/ewMath/ewMath.h>
@@ -15,11 +17,31 @@
 #include <ew/camera.h>
 #include <ew/cameraController.h>
 
+#define _USE_MATH_DEFINES
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void resetCamera(ew::Camera& camera, ew::CameraController& cameraController);
 
 int SCREEN_WIDTH = 1080;
 int SCREEN_HEIGHT = 720;
+
+constexpr int MAX_LIGHTS = 4;
+
+struct Light
+{
+	ew::Vec3 positon;
+	ew::Vec3 color = ew::Vec3(1.f);
+};
+
+void renderLight(const Light& light, ew::Shader& emissiveShader, const ew::Mesh& lightMesh)
+{
+	ew::Transform lightTransform;
+	lightTransform.position = light.positon;
+
+	emissiveShader.setMat4("_Model", lightTransform.getModelMatrix());
+	emissiveShader.setVec3("_lightColor", light.color);
+	lightMesh.draw();
+}
 
 float prevTime;
 ew::Vec3 bgColor = ew::Vec3(0.1f);
@@ -61,6 +83,8 @@ int main() {
 	ew::Shader shader("assets/defaultLit.vert", "assets/defaultLit.frag");
 	unsigned int brickTexture = ew::loadTexture("assets/brick_color.jpg",GL_REPEAT,GL_LINEAR);
 
+	ew::Shader emissiveShader("assets/emissive.vert", "assets/emissive.frag");
+
 	//Create cube
 	ew::Mesh cubeMesh(ew::createCube(1.0f));
 	ew::Mesh planeMesh(ew::createPlane(5.0f, 5.0f, 10));
@@ -76,6 +100,29 @@ int main() {
 	sphereTransform.position = ew::Vec3(-1.5f, 0.0f, 0.0f);
 	cylinderTransform.position = ew::Vec3(1.5f, 0.0f, 0.0f);
 
+	ew::Mesh lightMesh(ew::createSphere(0.3f, 12));
+
+	bool animateLights = true;
+	int activeLights = MAX_LIGHTS;
+	float lightOrbitRadius = 3.f;
+	float lightOrbitSpeed = 1.f;
+	float lightHeight = 3.f;
+	Light lights[MAX_LIGHTS] =
+	{
+		Light{ew::Vec3(0.f, lightHeight, lightOrbitRadius), ew::Vec3(1.f, 0.f, 0.f)},
+		Light{ew::Vec3(lightOrbitRadius, lightHeight, 0.f), ew::Vec3(0.f, 1.f, 0.f)},
+		Light{ew::Vec3(0.f, lightHeight, -lightOrbitRadius), ew::Vec3(0.f, 0.f, 1.f)},
+		Light{ew::Vec3(-lightOrbitRadius, lightHeight, 0.f), ew::Vec3(1.f, 1.f, 0)}
+	};
+
+	//Material properties
+	float ambientK = 0.2f;
+	ew::Vec3 ambientColor = ew::Vec3(0.341f, 0.365f, 0.51f);
+	float diffuseK = 0.4f;
+	float specularK = 0.5f;
+	float shininess = 10.f;
+
+
 	resetCamera(camera,cameraController);
 
 	while (!glfwWindowShouldClose(window)) {
@@ -89,7 +136,7 @@ int main() {
 		camera.aspectRatio = (float)SCREEN_WIDTH / SCREEN_HEIGHT;
 		cameraController.Move(window, &camera, deltaTime);
 
-		//RENDER
+	
 		glClearColor(bgColor.x, bgColor.y,bgColor.z,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -98,7 +145,20 @@ int main() {
 		shader.setInt("_Texture", 0);
 		shader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
 
-		//Draw shapes
+	
+		shader.setInt("_activeLights", activeLights);
+		for (int i = 0; i < activeLights; i++)
+		{
+			std::stringstream lightPositionStream;
+			lightPositionStream << "_lights[" << i << "].position";
+			std::stringstream lightColorStream;
+			lightColorStream << "_lights[" << i << "].color";
+
+			shader.setVec3(lightPositionStream.str(), lights[i].positon);
+			shader.setVec3(lightColorStream.str(), lights[i].color);
+		}
+
+
 		shader.setMat4("_Model", cubeTransform.getModelMatrix());
 		cubeMesh.draw();
 
@@ -111,7 +171,22 @@ int main() {
 		shader.setMat4("_Model", cylinderTransform.getModelMatrix());
 		cylinderMesh.draw();
 
-		//TODO: Render point lights
+		//Set material/light props
+		shader.setFloat("_material.ambientK", ambientK);
+		shader.setVec3("_ambientColor", ambientColor);
+		shader.setFloat("_material.diffuseK", diffuseK);
+		shader.setFloat("_material.specularK", specularK);
+		shader.setFloat("_material.shininess", shininess);
+
+		//Render point lights
+		//Setup emissive shader
+		emissiveShader.use();
+		emissiveShader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
+		//Render all lights
+		for (int i = 0; i < activeLights; i++)
+		{
+			renderLight(lights[i], emissiveShader, lightMesh);
+		}
 
 		//Render UI
 		{
